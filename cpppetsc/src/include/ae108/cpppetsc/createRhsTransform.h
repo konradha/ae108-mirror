6a050de07a9d2b1b88c2557b6fe1279428d71903
Manuel Weberndorfer <manuel.weberndorfer@id.ethz.ch>
2021-01-20 15:28:48 +0100

    add createRhsTransform

---
+++ cpppetsc/src/include/ae108/cpppetsc/createRhsTransform.h
@@ -0,0 +1,66 @@
+// © 2021 ETH Zurich, Mechanics and Materials Lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "ae108/cpppetsc/Matrix.h"
+#include "ae108/cpppetsc/ParallelComputePolicy_fwd.h"
+#include "ae108/cpppetsc/SequentialComputePolicy_fwd.h"
+#include "ae108/cpppetsc/UniqueEntity.h"
+
+namespace ae108 {
+namespace cpppetsc {
+
+/**
+ * @brief Creates a matrix P that can be multiplied from the right hand side
+ * with the provided Matrix A, i.e. A * P is well-formed.
+ */
+template <class Policy>
+Matrix<Policy>
+createRhsTransform(const Matrix<Policy> &matrix,
+                   const typename Matrix<Policy>::size_type columns) {
+  using size_type = typename Matrix<Policy>::size_type;
+
+  auto result = []() {
+    auto mat = Mat{};
+    Policy::handleError(MatCreate(Policy::communicator(), &mat));
+    return Matrix<Policy>(makeUniqueEntity<Policy>(mat));
+  }();
+
+  const auto localRows = [&]() {
+    auto rows = size_type{};
+    auto cols = size_type{};
+    Policy::handleError(MatGetLocalSize(matrix.data(), &rows, &cols));
+    return cols;
+  }();
+
+  const auto rows = matrix.size().second;
+
+  Policy::handleError(
+      MatSetSizes(result.data(), localRows, PETSC_DECIDE, rows, columns));
+  Policy::handleError(MatSetFromOptions(result.data()));
+  Policy::handleError(MatSetUp(result.data()));
+  result.finalize();
+  return result;
+}
+
+extern template Matrix<SequentialComputePolicy>
+createRhsTransform(const Matrix<SequentialComputePolicy> &matrix,
+                   typename Matrix<SequentialComputePolicy>::size_type);
+extern template Matrix<ParallelComputePolicy>
+createRhsTransform(const Matrix<ParallelComputePolicy> &matrix,
+                   typename Matrix<ParallelComputePolicy>::size_type);
+
+} // namespace cpppetsc
+} // namespace ae108
---
+++ cpppetsc/test/createRhsTransform_Test.cc
@@ -0,0 +1,68 @@
+// © 2021 ETH Zurich, Mechanics and Materials Lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "ae108/cpppetsc/ParallelComputePolicy.h"
+#include "ae108/cpppetsc/SequentialComputePolicy.h"
+#include "ae108/cpppetsc/copy.h"
+#include "ae108/cpppetsc/createRhsTransform.h"
+#include "ae108/cppptest/Matchers.h"
+#include <gmock/gmock.h>
+
+using testing::DoubleEq;
+using testing::Eq;
+using testing::Pair;
+using testing::Test;
+using testing::Types;
+
+namespace ae108 {
+namespace cpppetsc {
+
+template <class Policy> struct createRhsTransform_Test : Test {
+  using matrix_type = Matrix<Policy>;
+  using size_type = typename matrix_type::size_type;
+};
+
+using Policies = Types<SequentialComputePolicy, ParallelComputePolicy>;
+
+TYPED_TEST_CASE(createRhsTransform_Test, Policies);
+
+TYPED_TEST(createRhsTransform_Test, has_correct_size) {
+  using matrix_type = typename TestFixture::matrix_type;
+  using size_type = typename TestFixture::size_type;
+
+  const auto matrix = matrix_type(2, 3);
+
+  const auto columns = size_type{7};
+  const auto result = createRhsTransform(matrix, columns);
+
+  EXPECT_THAT(result.size(), Pair(Eq(matrix.size().second), Eq(columns)));
+}
+
+TYPED_TEST(createRhsTransform_Test, can_be_multiplied) {
+  using matrix_type = typename TestFixture::matrix_type;
+  using size_type = typename TestFixture::size_type;
+
+  auto matrix = matrix_type(2, 3);
+  matrix.setZero();
+
+  const auto columns = size_type{7};
+  auto transform = createRhsTransform(matrix, columns);
+  transform.setZero();
+
+  const auto result = matrix_type::fromProduct(matrix, transform);
+  EXPECT_THAT(result.norm(), DoubleEq(0.));
+}
+
+} // namespace cpppetsc
+} // namespace ae108

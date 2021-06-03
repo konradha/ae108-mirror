a9260406044959101fca0f6124b27a1eed698643
Manuel Weberndorfer <manuel.weberndorfer@id.ethz.ch>
2021-06-03 11:24:18 +0000

    add function asTransformedMatrix

---
+++ cpppetsc/src/include/ae108/cpppetsc/asTransformedMatrix.h
@@ -0,0 +1,169 @@
+// © 2021 ETH Zurich, Mechanics and Materials Lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "ae108/cpppetsc/Matrix.h"
+#include "ae108/cpppetsc/ParallelComputePolicy_fwd.h"
+#include "ae108/cpppetsc/SequentialComputePolicy_fwd.h"
+
+namespace ae108 {
+namespace cpppetsc {
+
+/**
+ * @brief Returns a matrix that behaves like the transformed matrix
+ * T * M * T^t.
+ *
+ * Note that the matrix is not actually computed, but the return matrix
+ * stores a reference to the given matrices to compute the result of
+ * operations on demand.
+ *
+ * @param matrix Valid nonzero pointer.
+ * @param transform Valid nonzero pointer.
+ */
+template <class Policy>
+Matrix<Policy> asTransformedMatrix(const Matrix<Policy> *matrix,
+                                   const Matrix<Policy> *transform);
+
+extern template Matrix<SequentialComputePolicy>
+asTransformedMatrix(const Matrix<SequentialComputePolicy> *,
+                    const Matrix<SequentialComputePolicy> *);
+extern template Matrix<ParallelComputePolicy>
+asTransformedMatrix(const Matrix<ParallelComputePolicy> *,
+                    const Matrix<ParallelComputePolicy> *);
+
+} // namespace cpppetsc
+} // namespace ae108
+
+#include "ae108/cpppetsc/createTransformInput.h"
+#include "ae108/cpppetsc/createTransformOutput.h"
+#include <cassert>
+#include <petscmat.h>
+#include <utility>
+
+namespace ae108 {
+namespace cpppetsc {
+
+namespace detail {
+namespace asTransformedMatrix {
+
+/**
+ * @brief Stores the data needed to compute the matrix by vector
+ * product. Also contains two vectors to store intermediate
+ * results.
+ */
+template <class Policy> struct Data {
+  Matrix<Policy> matrix;
+  Matrix<Policy> transform;
+
+  distributed<Vector<Policy>> x;
+  distributed<Vector<Policy>> y;
+};
+
+/**
+ * @brief Computes `transform * matrix * transform^t * in` using the
+ * matrices in the context.
+ */
+template <class Policy> PetscErrorCode multiply(Mat mat, Vec in, Vec out) {
+  const auto data = [&]() {
+    auto context = static_cast<Data<Policy> *>(nullptr);
+    Policy::handleError(MatShellGetContext(mat, &context));
+    return context;
+  }();
+
+  Policy::handleError(
+      MatMultTranspose(data->transform.data(), in, data->x.unwrap().data()));
+  Policy::handleError(MatMult(data->matrix.data(), data->x.unwrap().data(),
+                              data->y.unwrap().data()));
+  Policy::handleError(
+      MatMult(data->transform.data(), data->y.unwrap().data(), out));
+
+  return PetscErrorCode{};
+}
+
+/**
+ * @brief Deletes the context.
+ */
+template <class Policy> PetscErrorCode destroy(Mat mat) {
+  const auto data = [&]() {
+    auto context = static_cast<Data<Policy> *>(nullptr);
+    Policy::handleError(MatShellGetContext(mat, &context));
+    return context;
+  }();
+
+  delete data;
+
+  return PetscErrorCode{};
+}
+
+/**
+ * @brief Increases the reference count of the contained `Mat` of the given
+ * matrix and returns a matrix wrapping the result.
+ */
+template <class Policy>
+Matrix<Policy> shallowCopy(const Matrix<Policy> *matrix) {
+  assert(matrix);
+  Policy::handleError(PetscObjectReference((PetscObject)matrix->data()));
+  return Matrix<Policy>(makeUniqueEntity<Policy>(matrix->data()));
+}
+
+} // namespace asTransformedMatrix
+} // namespace detail
+
+template <class Policy>
+Matrix<Policy> asTransformedMatrix(const Matrix<Policy> *matrix,
+                                   const Matrix<Policy> *transform) {
+  assert(matrix);
+  assert(transform);
+
+  using size_type = typename Matrix<Policy>::size_type;
+
+  const auto localSize = [&]() {
+    auto size = std::pair<size_type, size_type>{};
+    Policy::handleError(
+        MatGetLocalSize(transform->data(), &size.first, &size.second));
+    return size;
+  }();
+
+  auto shell = [&]() {
+    auto mat = Mat{};
+    auto data = std::unique_ptr<detail::asTransformedMatrix::Data<Policy>>(
+        new detail::asTransformedMatrix::Data<Policy>{
+            detail::asTransformedMatrix::shallowCopy<Policy>(matrix),
+            detail::asTransformedMatrix::shallowCopy<Policy>(transform),
+            createTransformInput(*matrix), createTransformOutput(*matrix)});
+
+    Policy::handleError(MatCreateShell(Policy::communicator(), localSize.first,
+                                       localSize.first, PETSC_DETERMINE,
+                                       PETSC_DETERMINE, data.get(), &mat));
+
+    auto result = Matrix<Policy>(makeUniqueEntity<Policy>(mat));
+
+    Policy::handleError(MatShellSetOperation(
+        result.data(), MATOP_DESTROY,
+        (void (*)(void))detail::asTransformedMatrix::destroy<Policy>));
+    data.release();
+
+    return result;
+  }();
+
+  Policy::handleError(MatShellSetOperation(
+      shell.data(), MATOP_MULT,
+      (void (*)(void))detail::asTransformedMatrix::multiply<Policy>));
+
+  return shell;
+}
+
+} // namespace cpppetsc
+} // namespace ae108
